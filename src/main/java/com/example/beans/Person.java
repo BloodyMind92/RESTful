1a22d46cd681a9120d1f3dad1485088667f7c4f8
Monika Purwin <mpurwin@aiut.com.pl>
2018-08-20 13:31:47 +0200

    Stworzenie projektu, dodanie klas, rozszerzenie gitignore

---
+++ src/main/java/com/example/beans/Person.java
@@ -0,0 +1,45 @@
+package com.example.beans;
+
+import java.sql.Date;
+
+public class Person {
+
+	
+	private String name;
+	private String lastName;
+	private String sex;
+	private Date brithday;
+	private long PESEL;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBrithday() {
+		return brithday;
+	}
+	public void setBrithday(Date brithday) {
+		this.brithday = brithday;
+	}
+	public long getPESEL() {
+		return PESEL;
+	}
+	public void setPESEL(long pESEL) {
+		PESEL = pESEL;
+	}
+	
+	
+}
